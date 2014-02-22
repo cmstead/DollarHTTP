e02ebbb469541978f9caa7392d1c695d17eb4cb0
Chris Stead <cmstead@gmail.com>
2014-02-22 13:09:29 -0800

    First commit of code and directory structure. Updated readme.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 dollarhttp
 ==========
 
-DollarHttp is a full-spectrum cURL wrapper for PHP
+DollarHttp will be a full-spectrum request cURL wrapper for PHP.
+
+DollarHttp is intended to create a clean, easy way to interact with cURL and make calls across both HTTP and HTTPS
+via the four most common methods:
+
+- DELETE
+- GET
+- POST
+- PUT
+
+DollarHttp will support custom headers, POST and PUT body contents and a clear, method agnostic API for managing
+request arguments.
+
+Completed items are as follows:
+
+- [X] Object creation (accepted arguments: URL, request method, arguments, body)
+- [X] URL management
+- [X] Method management (get, set and clear -- defaults to GET on clear)
+- [X] Argument management (get, set, delete, clear)
+- [X] Custom header management (get, set, delete and clear)
+- [ ] Call body management (get, set and clear)
+- [ ] Argument construction upon executing request
+- [ ] Header construction upon executing request
+- [ ] HTTP and HTTPS support
+- [ ] Full API documentation
+
+
---
+++ src/dollarhttp.php
@@ -0,0 +1,70 @@
+<?php
+
+class DollarHttp{
+
+    protected $url = "";
+    protected $method = "GET";
+    protected $arguments = array();
+    protected $headers = array();
+    protected $body = "";
+
+    public function __construct($url = null, $method = null, $arguments = null, $body = null){
+
+    }
+
+    /* URL management */
+    public function getUrl(){
+        return $this->$url;
+    }
+
+    public function setUrl($url){
+        $this->url = $url;
+    }
+
+    public function clearUrl(){
+        $this->$url = "";
+    }
+
+    /* Method management */
+    public function getMethod(){
+        return $this->method;
+    }
+
+    //Todo: validate request method before setting.
+    public function setMethod($method){
+        $this->method = $method;
+    }
+
+    public function clearMethod(){
+        $this->method = "GET";
+    }
+
+    /* Argument management */
+    public function setArgument($key, $value){
+        $this->arguments[$key] = $value;
+    }
+
+    public function deleteArgument($key){
+        unset($this->arguments[$key]);
+    }
+
+    public function clearArguments(){
+        $this->arguments = array();
+    }
+
+    /* Header management */
+    public function setHeader($key, $value){
+        $this->headers[$key] = $value;
+    }
+
+    public function deleteHeader($key){
+        unset($this->headers[$key]);
+    }
+
+    public function clearHeaders(){
+        $this->headers = array();
+    }
+
+}
+
+?>
